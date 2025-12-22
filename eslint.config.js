d2bce482380caa3b0855e26e170b84835e9e455f
faizanalibaig <riazfiaz6181@gmail.com>
2025-12-22 17:41:29 +0500

    Setup spidey-portal [1]

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  },
+];
